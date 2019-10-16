a1fb7918a80319aaa97e467c666bcb61bb428218
Marianna Pinho <mariannapinho@alu.ufc.br>
2019-10-16 20:59:48 -0300

    Update adsb_userInfo.h

--- 04-Coletor_ADSB_THREAD/src/adsb_userInfo.h
+++ 04-Coletor_ADSB_THREAD/src/adsb_userInfo.h
@@ -7,7 +7,7 @@ associate the user with his/her
 collector.
 =================================*/
 
-#define COLLECTOR_KEY    "5980d171-73fb-46c1-aea1-e7b5d5c1de60"
-#define USER_AUTH   "admin3:RLadmin#"
+#define COLLECTOR_KEY    ""
+#define USER_AUTH   ""
 
 #endif
